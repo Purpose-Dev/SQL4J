87a59ed1dcebfb4939b471c9943b01f46906f6bc
Riyane El Qoqui <riyane.dev@gmail.com>
2025-09-23 22:50:01 +0200

    chore(sql4j): Add ColumnType enum for SQL data types

---
+++ sql4j-core/src/main/scala/sql4j/core/ColumnType.scala
@@ -0,0 +1,15 @@
+package sql4j.core
+
+enum ColumnType:
+		case ByteType
+		case ShortType
+		case IntType
+		case LongType
+		case FloatType
+		case DoubleType
+		case BooleanType
+		case CharType
+		case StringType
+		case BigDecimalType
+		case BigIntType
+		case BinaryType
