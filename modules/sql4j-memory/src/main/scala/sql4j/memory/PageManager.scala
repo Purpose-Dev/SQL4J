690fd2d6e1c75bbd35fd7fc9d4e06af2538bbbc5
Riyane El Qoqui <riyane.dev@gmail.com>
2025-09-26 23:20:11 +0200

    feat(sql4j-memory/PageManager): Implement PageManager class for managing page allocation and lifecycle

---
+++ modules/sql4j-memory/src/main/scala/sql4j/memory/PageManager.scala
@@ -0,0 +1,28 @@
+package sql4j.memory
+
+import sql4j.memory.page.PageHeader
+
+import java.nio.ByteBuffer
+import scala.collection.mutable
+
+class PageManager(pool: MemoryPool):
+		private val pages = mutable.ListBuffer[(ByteBuffer, PageHeader)]()
+
+		def newPage(): (ByteBuffer, PageHeader) =
+				val buf = pool.allocatePage()
+				val header = PageHeader(buf)
+				header.init()
+				pages.append((buf, header))
+				(buf, header)
+
+		def findPage(required: Int): Option[(ByteBuffer, PageHeader)] =
+				pages.find {
+						case (_, header) => header.canFit(required)
+				}
+
+		def releasePage(buf: ByteBuffer): Unit =
+				pages.indexWhere(_._1 eq buf) match
+						case -1 => ()
+						case i =>
+								pages.remove(i)
+								pool.releasePage(buf)
