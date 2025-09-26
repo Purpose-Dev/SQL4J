d98c456c26ea39626c1b88f8602b2bf9baf30663
Riyane El Qoqui <riyane.dev@gmail.com>
2025-09-26 23:19:55 +0200

    feat(sql4j-memory/MemoryPool): Implement MemoryPool class for efficient page allocation and management

---
+++ modules/sql4j-memory/src/main/scala/sql4j/memory/MemoryPool.scala
@@ -0,0 +1,24 @@
+package sql4j.memory
+
+import sql4j.memory.off_heap.PageLayout
+
+import java.nio.ByteBuffer
+import scala.collection.mutable
+
+class MemoryPool(totalPages: Int):
+		private val freePages = mutable.Stack[ByteBuffer]()
+		private val allPages = Array.fill(totalPages)(ByteBuffer.allocateDirect(PageLayout.PageSize))
+
+		allPages.foreach(freePages.push)
+
+		def allocatePage(): ByteBuffer =
+				if freePages.isEmpty then
+						throw new RuntimeException("MemoryPool exhausted")
+				else
+						freePages.pop()
+
+		def releasePage(buf: ByteBuffer): Unit =
+				buf.clear()
+				freePages.push(buf)
+
+		def availablePages: Int = freePages.size
