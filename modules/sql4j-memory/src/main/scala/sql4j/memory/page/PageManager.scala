405200f5db47916fa7f816a6457645f244e58d85
Riyane El Qoqui <riyane.dev@gmail.com>
2025-09-28 13:47:02 +0200

    feat(sql4j-memory/page): Implement PageEntry and PageManager for enhanced page management

--- modules/sql4j-memory/src/main/scala/sql4j/memory/page/PageManager.scala
+++ modules/sql4j-memory/src/main/scala/sql4j/memory/page/PageManager.scala
@@ -1,28 +1,57 @@
 package sql4j.memory.page
 
+import sql4j.core.PageId
 import sql4j.memory.MemoryPool
 
 import java.nio.ByteBuffer
-import scala.collection.mutable
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.atomic.AtomicLong
 
 class PageManager(pool: MemoryPool):
-		private val pages = mutable.ListBuffer.empty[(ByteBuffer, PageHeader)]
+		private val idGenerator = AtomicLong(1L)
+		private val directory = ConcurrentHashMap[PageId, PageEntry]()
 
-		def newPage(): (ByteBuffer, PageHeader) =
+		private def get(pageId: PageId): Option[PageEntry] =
+				Option(directory.get(pageId))
+
+		def newPage(): PageEntry =
 				val buf = pool.allocatePage()
-				val header = PageHeader(buf)
-				header.init()
-				pages.append((buf, header))
-				(buf, header)
+				PageHeader(buf).init()
+				val id = PageId(idGenerator.getAndIncrement())
+				val pm = PageMeta()
+				val entry = PageEntry(id, buf, pm)
+				directory.put(id, entry)
+				entry
 
-		def findPage(required: Int): Option[(ByteBuffer, PageHeader)] =
-				pages.find {
-						case (_, header) => header.canFit(required)
-				}
+		def pin(pageId: PageId): Option[ByteBuffer] =
+				get(pageId) match
+						case Some(entry) =>
+								if entry.tryPin() then
+										Some(entry.buffer)
+								else
+										None
+						case None => None
 
-		def releasePage(buf: ByteBuffer): Unit =
-				pages.indexWhere(_._1 eq buf) match
-						case -1 => ()
-						case i =>
-								pages.remove(i)
-								pool.releasePage(buf)
+		def unpin(pageId: PageId): Unit =
+				get(pageId).foreach(_.tryUnpin())
+
+		def free(pageId: PageId): Boolean =
+				get(pageId) match
+						case None => false
+						case Some(entry) =>
+								if entry.meta.pinnedCount() == 0 then
+										val removed = directory.remove(pageId)
+										if removed ne null then
+												pool.releasePage(entry.buffer)
+												true
+										else
+												false
+								else
+										false
+
+		def foreachEntry(f: PageEntry => Unit): Unit =
+				val it = directory.values().iterator()
+				while it.hasNext do
+						f(it.next())
+
+		def currentCount(): Int = directory.size()
