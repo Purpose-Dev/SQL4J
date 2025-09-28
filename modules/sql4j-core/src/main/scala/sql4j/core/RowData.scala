4c7fd4cdf636299ee392e47ca4afac2c6b76fe0b
Riyane El Qoqui <riyane.dev@gmail.com>
2025-09-28 08:26:52 +0200

    feat(RowData): Add factory method to create RowData from an existing map

--- modules/sql4j-core/src/main/scala/sql4j/core/RowData.scala
+++ modules/sql4j-core/src/main/scala/sql4j/core/RowData.scala
@@ -5,3 +5,10 @@ import scala.reflect.ClassTag
 case class RowData(values: Map[String, Any]):
 		def get[T: ClassTag](column: String): Option[T] =
 				values.get(column).collect { case v: T => v }
+
+object RowData:
+		def apply(origin: Map[String, Any]): RowData =
+				val copy = origin.foldLeft(Map.empty[String, Any]) {
+						case (acc, (k, v)) => acc + (k -> v)
+				}
+				new RowData(copy)
