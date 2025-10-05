b70c63ccdf834a5c5e5ec7f0aa795e6cb6af68ab
Riyane El Qoqui <riyane.dev@gmail.com>
2025-10-05 13:15:18 +0200

    feat(sql4j-memory/page/reclaimer): Add PageReclaimer for managing page memory reclamation

---
+++ modules/sql4j-memory/src/main/scala/sql4j/memory/page/PageReclaimer.scala
@@ -0,0 +1,51 @@
+package sql4j.memory.page
+
+import sql4j.memory.MemoryPool
+import sql4j.memory.off_heap.PageLayout
+import zio.{durationLong, Ref, UIO, ZIO, ZLayer}
+
+// @formatter:off
+final case class PageReclaimerConfig(
+		scanIntervalMs: Long = 500L,
+		maxPagesPerScan: Int = 100
+)
+// @formatter:on
+
+trait PageReclaimer:
+		def start: UIO[Unit]
+
+		def stop: UIO[Unit]
+
+object PageReclaimer:
+		def live(pm: PageManager, pool: MemoryPool, cfg: PageReclaimerConfig = PageReclaimerConfig()): ZLayer[Any, Nothing, PageReclaimer] =
+				ZLayer.scoped {
+						for {
+								running <- Ref.make(true)
+								fiber <- (reclaimerLoop(pm, pool, running, cfg)).forever.forkScoped
+						} yield new PageReclaimer:
+								override def start: UIO[Unit] = running.set(false)
+
+								override def stop: UIO[Unit] = running.set(false)
+				}
+
+		private def reclaimerLoop(pm: PageManager, pool: MemoryPool, running: Ref[Boolean], cfg: PageReclaimerConfig): ZIO[Any, Nothing, Unit] =
+				for {
+						r <- running.get
+						_ <- if !r then
+								ZIO.yieldNow
+						else {
+								val candidates = pm.snapshotEntries().take(cfg.maxPagesPerScan)
+								ZIO.foreachDiscard(candidates) { entry =>
+										val header = PageHeader(entry.buffer)
+										val nEntries = header.getNEntries
+										val freePtr = header.getFreeSpacePointer
+
+										if nEntries == 0 && freePtr == PageLayout.PageSize && entry.meta.pinnedCount() == 0 then
+												ZIO.attempt {
+														pm.free(entry.id)
+												}.catchAll(_ => ZIO.unit)
+										else
+												ZIO.unit
+								}
+						} *> ZIO.sleep(cfg.scanIntervalMs.millis)
+				} yield ()
