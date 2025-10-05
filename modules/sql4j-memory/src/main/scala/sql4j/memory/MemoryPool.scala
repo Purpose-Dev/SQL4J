26522d676d6d340a0bf3e43b9c46acdb158504bd
Riyane El Qoqui <riyane.dev@gmail.com>
2025-10-05 15:29:21 +0200

    refactor(sql4j-memory): Move MemoryPoolMetrics case class to MemoryPool.scala

--- modules/sql4j-memory/src/main/scala/sql4j/memory/MemoryPool.scala
+++ modules/sql4j-memory/src/main/scala/sql4j/memory/MemoryPool.scala
@@ -5,6 +5,15 @@ import sql4j.memory.off_heap.PageLayout
 import java.nio.ByteBuffer
 import scala.collection.mutable
 
+// @formatter:off
+final case class MemoryPoolMetrics(
+				totalPages: Int,
+				freePages: Int,
+				allocatedPages: Int,
+				pageSizeBytes: Int
+)
+// @formatter:on
+
 class MemoryPool(val totalPages: Int):
 		private val freePages = mutable.Stack[ByteBuffer]()
 		private val allPages = Array.fill(totalPages)(ByteBuffer.allocateDirect(PageLayout.PageSize))
